59922310c6b61582d5d00c11e4d18efaca38c22b
ahmadsaputra19 <rebahankan22@gmail.com>
2024-07-04 15:31:57 +0700

    menambahkan menu search

--- script.js
+++ script.js
@@ -31,3 +31,31 @@ document.getElementById('whatsapp-form').addEventListener('submit', function(eve
     
     window.open(whatsappURL, '_blank');
 });
+
+
+//Search Dinamis
+document.getElementById("search-input").addEventListener("input", function() {
+    var searchInput = this.value.trim().toLowerCase();
+    var resultsContainer = document.getElementById("searchResults");
+    resultsContainer.innerHTML = ""; // Clear previous results
+
+    if (searchInput === "") {
+        return; // Exit if the search input is empty
+    }
+
+    var divs = document.querySelectorAll(".content-div");
+
+    for (var i = 0; i < divs.length; i++) {
+        var div = divs[i];
+        var divId = div.getAttribute("id").toLowerCase();
+
+        if (divId.includes(searchInput)) {
+            var resultItem = document.createElement("div");
+            resultItem.textContent = divId;
+            resultItem.addEventListener("click", function() {
+                document.getElementById(this.textContent).scrollIntoView({ behavior: 'smooth' });
+            });
+            resultsContainer.appendChild(resultItem);
+        }
+    }
+});

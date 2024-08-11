50bdafa1ab1114b622d23cdd652ce7d454d82471
ahmadsaputra19 <rebahankan22@gmail.com>
2024-08-11 17:05:32 +0700

    menambahkan notifikasi pada sw.js

--- sw.js
+++ sw.js
@@ -18,7 +18,21 @@ self.addEventListener("fetch",(e)=>{
     
     try {
       const res = await fetch(e.request)
-      
+
+      //notifilasi lirik baru
+      if (e.request.url.endsWith('./index.html')) {
+        const oldLyrics = await resCache ? await resCache.text() : '';
+        const newLyrics = await res.clone().text();
+
+        if (oldLyrics !== newLyrics) {
+          self.registration.showNotification('Ada Lirik Baru Ditambahkan!', {
+            body: 'Kami Telah Menambahkan List Sholawat Baru, Silahkan Bisa Update!',
+            icon: './aamedia.png', // Sesuaikan path ikon
+            tag: 'update-notification'
+          });
+        }
+      }
+
       cache.put(e.request,res.clone())
       return res
     } catch (error){
@@ -26,3 +40,13 @@ self.addEventListener("fetch",(e)=>{
     }
   })())
 })
+
+
+// Event Notification Click - menangani klik notifikasi
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('./index.html') // Arahkan ke halaman lirik baru
+  );
+});
